ae59d1fb2b15c4b8255ea288d4eca58c3150df8f
JackRFuller <jackrfuller@outlook.com>
2015-10-08 17:00:52 +0100

    Basic Enemy Implementation
    - Enemy Class Implementation
    - Enemy now follows waypoints via nav mesh

---
+++ Cover_Unity/Assets/Scripts/Behaviours/RifleEnemyBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RifleEnemyBehaviour : BaseEnemyClass {
+
+	// Use this for initialization
+	void Start () {
+
+		navMeshAgent.speed = walkSpeed;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(CurrentMode == Mode.Patrol)
+		{
+			Patrol();
+		}
+
+
+	
+	}
+
+	void Patrol()
+	{
+		navMeshAgent.SetDestination(patrollingWaypoints[waypointID].position);
+
+		float _distance = Vector3.Distance(transform.position, patrollingWaypoints[waypointID].position);
+
+		if(_distance < 1)
+		{
+			waypointID++;
+			if(waypointID == patrollingWaypoints.Length)
+			{
+				waypointID = 0;
+			}
+		}
+
+	}
+}
---
+++ Cover_Unity/Assets/Scripts/Classes/BaseEnemyClass.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseEnemyClass : MonoBehaviour {
+
+	public enum Mode
+	{
+		Patrol,
+		Attack
+	}
+
+	public Mode CurrentMode;
+
+	[Header("Movement Items")]
+	public NavMeshAgent navMeshAgent;
+	public float walkSpeed;
+	public float runSpeed;
+
+	[Header("Targets")]
+	public Transform[] patrollingWaypoints;
+	public int waypointID = 0;
+
+	[Header("Detecting Player")]
+	public float fieldofViewAngle = 110F;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
+	public Vector3 previousSighting;
+	 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
