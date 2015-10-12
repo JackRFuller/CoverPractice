d91fba94bc16d744cf9e1357c83f96780c0c7ac4
JackRFuller <jackrfuller@outlook.com>
2015-10-12 23:01:07 +0100

    Gun Model
    - Added Gun Model
    - Created Animator Controller
    - Started structuring game behaviour script

---
+++ Cover_Unity/Assets/Scripts/Behaviours/GunAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunAnimation : MonoBehaviour {
+
+    [SerializeField] private Animator gunAnimator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Reload()
+    {
+        gunAnimator.SetBool("Reload", true);
+    }
+}
---
+++ Cover_Unity/Assets/Scripts/Behaviours/PlayerShootBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShootBehaviour : MonoBehaviour {
+
+    [Header("Managers")]
+    [SerializeField] private GunAnimation GA_Script;
+    
+    [SerializeField] private Transform gunTransform;    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            CheckAmmo();
+        }
+	
+	}
+
+    void CheckAmmo()
+    {
+        Reload();
+    }
+
+    void Reload()
+    {
+        GA_Script.Reload();
+    }
+
+    void SetGunPosition()
+    {
+      
+        
+    }
+}
