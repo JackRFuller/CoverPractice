b0373efede59f1c0493923623b1952c554928bf3
JackRFuller <jackrfuller@outlook.com>
2015-10-12 16:56:06 +0100

    Level Building Blocks
    - The building blocks have been imported

--- Cover_Unity/Assets/Scripts/Behaviours/RifleEnemyBehaviour.cs
+++ Cover_Unity/Assets/Scripts/Behaviours/RifleEnemyBehaviour.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class RifleEnemyBehaviour : BaseEnemyClass {
 
+	[Header("Detection Zone")]
+	[SerializeField] private BoxCollider detectionZone;
+	[SerializeField] private Vector3 detectionZoneSize;
+
+
 	// Use this for initialization
 	void Start () {
 
 		navMeshAgent.speed = walkSpeed;
 	
+	}
+
+	void InitialValues()
+	{
+		detectionZone.size = detectionZoneSize;
 	}
 	
 	// Update is called once per frame
@@ -28,7 +38,7 @@ public class RifleEnemyBehaviour : BaseEnemyClass {
 
 		float _distance = Vector3.Distance(transform.position, patrollingWaypoints[waypointID].position);
 
-		if(_distance < 1)
+		if(_distance < 2)
 		{
 			waypointID++;
 			if(waypointID == patrollingWaypoints.Length)
@@ -37,5 +47,13 @@ public class RifleEnemyBehaviour : BaseEnemyClass {
 			}
 		}
 
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			CurrentMode = Mode.Attack;
+		}
 	}
 }
