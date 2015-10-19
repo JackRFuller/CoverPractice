d086752e3d9af92ae105a852b96325380b34bfe0
JackRFuller <jackrfuller@outlook.com>
2015-10-19 14:00:14 +0100

    Redraft Combat Implementation
    - Combat has been redrafted with the old system being deleted
    - Enemies now move to position when combat has been initiated/they have
    been shot

---
+++ Cover_Unity/Assets/Scripts/Behaviours/Combat/CombatZoneBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatZoneBehaviour : MonoBehaviour {
+
+
+	[Header("Status")]
+	[SerializeField] private bool isActive = false;
+	[SerializeField] private bool isWave1Over = false;
+	[SerializeField] private bool isWave2Over = false;
+
+	[Header("Soldiers")]
+	[SerializeField] private EnemyCombatBehaviour[] ECB_Scripts;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			if(!isActive)
+			{
+				SetSoldiersIntoCombat();
+			}
+		}
+
+	}
+
+	public void SetSoldiersIntoCombat()
+	{
+		foreach(EnemyCombatBehaviour ECB_Script in ECB_Scripts)
+		{
+			ECB_Script.inCombat = true;
+			isActive = true;
+		}
+	}
+}
--- Cover_Unity/Assets/Scripts/Behaviours/Enemies/EnemyCombatBehaviour.cs
+++ Cover_Unity/Assets/Scripts/Behaviours/Enemies/EnemyCombatBehaviour.cs
@@ -1,241 +1,133 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[RequireComponent (typeof(NavMeshAgent))]
 public class EnemyCombatBehaviour : MonoBehaviour {
 
-    [Header("Managers")]
-    [SerializeField] private EnemyAnimationManager EAM_Script;
-    [SerializeField] private EnemyUIManager EUM_Script;
-    [SerializeField] private EnemyGunAnimation EGA_Script;
-    [SerializeField] private EnemyShootBehaviour ESB_Script;
+	[Header("Combat Manager")]
+	[SerializeField] private CombatZoneBehaviour CZB_Script;
 
-    [Header("Typing")]
-    [SerializeField] private string EnemyName;
-    public enum EnemyType
-    {
-        Idle,
-        Patrol
-    }
-    public EnemyType currentEnemyType;
-    public enum CombatMode
-    {
-        Passive,
-        Aggressive,
-        Searching,
-    }
-    public CombatMode currentCombatMode;
-    public enum AggressiveType
-    {
-        InPosition,
-        FindCover,
-    }
-    public AggressiveType currentAggroType;
+	[Header("Status")]
+	public bool inCombat = false;
 
-    [Header("Movement")]
-    [SerializeField] private float patrollingSpeed;
-    [SerializeField] private float runToCoverSpeed;
+	[System.Serializable]
+	public class EnemyType
+	{
+		public enum SoldierWave
+		{
+			Wave1,
+			Wave2,
+		}
+		public SoldierWave Wave;
 
-    [Header("Patrolling Items")]
-    [SerializeField] private Transform[] patrolWaypoints;
-    private int waypointTarget;
+		public enum Weapon
+		{
+			SMG,
+			Sniper,
+		}
 
-    [Header("Navigation")]
-    [SerializeField] private NavMeshAgent navMeshAgent;
+		public Weapon WeaponType;
 
-    [Header("Cover")]
-    [SerializeField] private Transform designatedCover;
-    private float originalColliderHeight;
-    private Vector3 originalColliderPos;
-    [SerializeField] private float coverColliderHeight;
-    [SerializeField] private Vector3 coverColliderPos;
+		public enum CoreAction
+		{
+			HideInCover,
+			StandAndShoot,
+			RunAndGun,
+		}
+
+		public CoreAction Action;
+	}
+
+	public EnemyType EnemySettings;
+
+	[Header("Cover")]
+	[SerializeField] private bool isInCover;
+	public Transform allocatedCover;
+	[SerializeField] private float inCoverLimit;
+
+	[Header("Shooting")]
+	[SerializeField] private bool hasLocatedTarget = false;
+	public Transform shootingTarget;
+
+	[Header("Movement")]
+	[SerializeField] private NavMeshAgent navMeshAgent;
+	[SerializeField] private float runningSpeed;
 
 
-    [Header("Life")]
-    [SerializeField] private float health;
+	// Use this for initialization
+	void Start () {
 
-    [Header("Misc")]
-    [SerializeField] private Transform targetPC;
-    [SerializeField] private Transform soldierMesh;
-    [SerializeField] private CapsuleCollider mainCollider;
-    private bool isDead;
-
-    // Use this for initialization
-    void Start () {
-
-        IntialValues();
+		InitialValues();
 	
 	}
 
-    void IntialValues()
-    {
-        //Managers
-        ESB_Script = this.GetComponent<EnemyShootBehaviour>();
-
-        mainCollider.enabled = true;
-        originalColliderHeight = mainCollider.height;
-        originalColliderPos = mainCollider.center;        
-
-        if(currentEnemyType == EnemyType.Patrol)
-        {
-            navMeshAgent.speed = patrollingSpeed;
-        }
-    }
+	void InitialValues()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		shootingTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (!isDead)
-        {
-            DetermineEnemyType();
-
-            if (currentCombatMode == CombatMode.Aggressive)
-            {
-                if (currentAggroType == AggressiveType.InPosition)
-                {
-                    LocateEnemy();
-                }
-                if (currentAggroType == AggressiveType.FindCover)
-                {
-                    MoveToCover();
-                }
-
-            }
-        } 
-
-        
+		if(inCombat)
+		{
+			EngageInCombat();
+		}
+	
 	}
 
-    void LocateEnemy()
-    {
-        Vector3 _targetPostition = new Vector3(targetPC.position.x, transform.position.y, targetPC.position.z);       
-        transform.LookAt(_targetPostition);        
-    }
+	public void EngageInCombat()
+	{
+		switch(EnemySettings.Wave)
+		{
 
-    void MoveToCover()
-    {
-        navMeshAgent.SetDestination(designatedCover.position);
+		case(EnemyType.SoldierWave.Wave1):
 
-        float _distance = Vector3.Distance(designatedCover.position, transform.position);
+			if(EnemySettings.Action == EnemyType.CoreAction.HideInCover || EnemySettings.WeaponType == EnemyType.Weapon.Sniper)
+			{
+				if(!isInCover)
+				{
+					MoveInToCover();
+				}
+				else
+				{
 
-        if(_distance < 0.5)
-        {
-            currentAggroType = AggressiveType.InPosition;
-            EAM_Script.HideBehindCover();
+				}
 
-            Crouching();
-        }
-    }
+			}
 
-    void DetermineEnemyType()
-    {
-        if(currentEnemyType == EnemyType.Patrol)
-        {
-            if(currentCombatMode == CombatMode.Passive)
-            {
-                Patrol();
-            }
-        }
-    }
+			break;
 
-    void Patrol()
-    {
-        navMeshAgent.SetDestination(patrolWaypoints[waypointTarget].position);
-        EAM_Script.Patrol();
 
-        float _distance = Vector3.Distance(patrolWaypoints[waypointTarget].position, transform.position);
+		case(EnemyType.SoldierWave.Wave2):
 
-        if(_distance < 0.5)
-        {
-            waypointTarget++;
+			if(!isInCover)
+			{
+				MoveInToCover();
+			}
+			break;
+		}
+	}
 
-            if(waypointTarget == patrolWaypoints.Length)
-            {
-                waypointTarget = 0;
-            }
-        }
-    }
+	public void MoveInToCover()
+	{
+		navMeshAgent.SetDestination(allocatedCover.position);
 
-     
+		float _distToCover = Vector3.Distance(allocatedCover.position, transform.position);
 
-    void Hit(float _damage)
-    {
-        if(currentCombatMode != CombatMode.Aggressive)
-        {
-            SetAggressive("shot");
-        }
+		if(_distToCover <= inCoverLimit)
+		{
+			isInCover = true;
+		}
+	}
 
-        health -= _damage;
+	void Hit(float _damage)
+	{
+		if(!inCombat)
+		{
+			CZB_Script.SetSoldiersIntoCombat();
+		}
+	}
 
-        //Update Health Bar
-        EUM_Script.HealthUpdate(health);
 
-        if(health <= 0)
-        {            
-            Dead();
-        }
-    }
-
-    public void SetAggressive(string _hitType)
-    {
-        currentCombatMode = CombatMode.Aggressive;       
-
-        if(_hitType == "shot")
-        {
-            SetStandingAim();
-        }
-        if(_hitType == "detected")
-        {
-            currentAggroType = AggressiveType.FindCover;
-            navMeshAgent.speed = runToCoverSpeed;
-
-            //--- Run to Cover Animations
-            EAM_Script.RunToCover();
-            EGA_Script.RunToCover();
-        }
-    }
-
-    void SetStandingAim()
-    {
-        navMeshAgent.speed = 0;
-        int _random = Random.Range(1, 3);
-        if (_random == 1)
-        {
-            EAM_Script.StandingAim();
-            EGA_Script.StandingAim();
-        }
-        else
-        {
-            EAM_Script.HideBehindCover();
-            
-            Crouching();
-        }
-    }
-
-    void Crouching()
-    {
-        EGA_Script.Crouching();
-        ESB_Script.StartShooting();
-
-        mainCollider.height = coverColliderHeight;
-        mainCollider.center = coverColliderPos;
-       
-    }
-
-    void Standing()
-    {
-        mainCollider.height = originalColliderHeight;
-        mainCollider.center = originalColliderPos;
-
-        ESB_Script.StartShooting();
-    }
-
-    void Dead()
-    {
-        isDead = true;
-        navMeshAgent.speed = 0;
-        mainCollider.enabled = false;
-
-        EAM_Script.Dead();
-        EGA_Script.Dead();
-    }
 }
